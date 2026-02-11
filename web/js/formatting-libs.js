2396c834c5b523f8df518698abe86ca884edf666
grahamstark <graham.stark@virtual-worlds.biz>
2026-02-11 10:58:20 +0000

    Graphics test.

--- web/js/formatting-libs.js
+++ web/js/formatting-libs.js
@@ -186,7 +186,7 @@ function make_media_item( colour, id, title, content, graph ){
                     id='${id}-image-thumbnail'
                     src="${graph}"
                     width='100'
-                    height='80'></div>
+                    height='80'></div>im
                 <div class="flex-grow-1 ms-3 ">
                     <h5 >${title}</h5>
                     <div class=''>${content}</div>
