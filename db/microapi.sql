22ce74e2cf46909765f9a7e560541f9822003a69
grahamstark <graham.stark@virtual-worlds.biz>
2026-03-12 15:13:45 +0000

    Start of DB backed version

--- db/microapi.sql
+++ db/microapi.sql
@@ -12,7 +12,9 @@ create table users(
     creation timestamp,
     is_temp boolean default true,
     primary key(username));
-insert into users values( 'default', '', md5('anything'), now(), false ) ;
+insert into users values
+( 'default', '', md5('anything'), now(), false ),
+( 'archive', '', md5('anything'), now(), false );
 
 create table models(
     model_name char(20) not null primary key,
@@ -21,15 +23,15 @@ insert into models values( 'scotben', '');
 
 create table model_versions(
     model_name char(20) not null,
-    model_version decimal(4,2) default 0.10,
+    model_version char(12) not null default '0.10',
     description text,
     primary key( model_name, model_version ),
     foreign key( model_name ) references models on delete cascade );
-insert into model_versions values('scotben',0.10,'');
+insert into model_versions values('scotben', '0.10','');
 
 create table sessions(
     username char(20) not null default 'default',
-    session_id serial,
+    session_id int not null default 1,
     creation_time timestamp,
     primary key( username, session_id),
     foreign key( username ) references users on delete cascade);
@@ -37,25 +39,38 @@ create table sessions(
 create table runs(
     username char(20) not null default 'default',
     model_name char(20) not null default 'scotben',
-    model_version decimal(4,2) not null default 0.10,
-    session_id integer not null,
+    model_version char(12) not null default '0.10',
+    session_id integer not null default 1,
     run_id serial,
     phase text not null,
     completed integer default 0,
     todo integer,
     submission timestamp,
-    params_json text not null,
-    settings_json text not null,
     primary key( username, session_id, run_id, model_name, model_version ),
     foreign key( username, session_id ) references sessions on delete cascade,
     foreign key( model_name, model_version) references model_versions );
 
-create table cached_results(
-    params_json text not null,
-    settings_json text not null,
-    result_type char(20) not null,
+create table run_params(
+    username char(20) not null default 'default',
     model_name char(20) not null default 'scotben',
-    model_version decimal(4,2) not null default 0.10,
-    result text,
-    primary key( params_json, settings_json, model_version),
-    foreign key( model_name, model_version) references model_versions on delete cascade)
+    model_version char(12) not null default '0.10',
+    session_id integer not null default 1,
+    run_id int not null,
+    name char(30) not null,
+    data text,
+    primary key( username, session_id, run_id, model_name, model_version, name ),
+    foreign key( username, session_id, run_id, model_name, model_version ) references runs on delete cascade on update cascade,
+    foreign key( model_name, model_version) references model_versions );
+
+create table run_results(
+    username char(20) not null default 'default',
+    model_name char(20) not null default 'scotben',
+    model_version char(12) not null default '0.10',
+    session_id integer not null default 1,
+    run_id int not null,
+    name char(30) not null,
+    datatype char(30) not null default 'json',
+    data text,
+    primary key( username, session_id, run_id, model_name, model_version, name ),
+    foreign key( username, session_id, run_id, model_name, model_version ) references runs on delete cascade on update cascade,
+    foreign key( model_name, model_version) references model_versions );
