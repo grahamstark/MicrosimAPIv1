b016deab0b719392fff60806a420208ebc14d555
Graham Stark <graham.stark@virtual-worlds.biz>
2026-06-03 22:06:10 +0100

    Rationalising imports..

--- src/MicrosimAPIv1.jl
+++ src/MicrosimAPIv1.jl
@@ -1,32 +1,15 @@
 module MicrosimAPIv1
 
-using Genie
 
-# using GenieSession 
-using Genie.Requests
-# using GenieSessionFileSession
-import Genie.Renderer.Json: json
-using Jedis
-
-
-include( "ScotbenAPIImpl.jl")
-include( )
-export ScotbenAPIImpl
-
-const up = Genie.up
-export up
-
-function main()
-    Genie.genie(; context = @__MODULE__)
-end
+include( "uses.jl")
 
 function __init__()
-    global t
-    t = @async ScotbenAPIImpl.calc_one()
-    for i in 1:ScotbenAPIImpl.NUM_HANDLERS # start n tasks to process requests in parallel
-        @info "starting handler $i" 
-        ScotbenAPIImpl.errormonitor(t)
-    end
+    # global t
+    # t = @async ScotbenAPIImpl.calc_one()
+    # for i in 1:ScotbenAPIImpl.NUM_HANDLERS # start n tasks to process requests in parallel
+    # @info "starting handler $i"
+    # ScotbenAPIImpl.errormonitor(t)
+    # end
 end
 
 end
