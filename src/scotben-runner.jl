5e926735de4fe93fb6ea9a92685e241f2767a9cd
grahamstark <graham.stark@virtual-worlds.biz>
2026-06-29 12:34:09 +0100

    Slowly filling in API implementations.

--- src/scotben-runner.jl
+++ src/scotben-runner.jl
@@ -1,10 +1,10 @@
-
 function grab_runs_from_queue(i::Integer)
     while true
         run = next_runnable_run()
         if ! isnothing( run )
             @info "run $(run.run_id) for user $(run.user_id) started in handler $(i)"
-
+        else
+            @info "no runnable runs for handler $(i)"
         end
     end
 end
@@ -14,7 +14,7 @@ const NUM_HANDLERS = 3 #
 #
 # Run the job queues
 #
-function start_scotben_queue(num_handlers=NUM_HANDLERS)
+function start_scotben_queues(num_handlers=NUM_HANDLERS)
     for i in 1:num_handlers # start n tasks to process requests in parallel
         @info "starting handler $i"
         errormonitor( @async grab_runs_from_queue(i))
