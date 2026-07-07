2ad90fd406094edda93eef065b0bc0f2315a08dd
Graham Stark <graham.stark@virtual-worlds.biz>
2026-07-07 08:08:53 +0100

    Painful progress towards everything running on /opt/api/.

--- bin/api-updater.sh
+++ bin/api-updater.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # use with su apiuser -c "bin/api-updater.sh"
 cd /opt/api/package/MicrosimAPIv1/
-/opt/api/julia/bin/julia --project=. -t auto -c "using Pkg;Pkg.activate(".");Pkg.instantiate();Pkg.update()"
+/opt/api/julia/bin/julia --project=. -t auto -e 'using Pkg;Pkg.activate(".");Pkg.instantiate();Pkg.update()'
 
